ef79acbe714962fe2d1e53e2c1c8c2393ff4a6bd
Max069 <misstatudi@gmail.com>
2021-08-14 06:11:25 -0700

    Gussa

--- Pig.js
+++ Pig.js
@@ -2,10 +2,20 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+    this.visibility= 255
   }
 display()
 {
   console.log(this.body.speed)
-
+  if(this.body.speed>3){
+    World.remove(world,this.body)
+    push()
+    tint(225, this.visibility)
+    super.display()
+    this.visibility-=5
+    pop()
+  }
+  else{
 super.display();
+  }
 }};
